a62e60ac98637c75e756d47f5bba622610d76278
Nelson Diaz <abnerdc86@gmail.com>
2022-09-27 03:05:37 -0400

    Create car object and animate it

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Self Driving Car
 
 Self driving car using javascript
+
+## Link to the tutorial
+
+https://www.youtube.com/watch?v=Rs_rAxEsAvI
---
+++ controls.js
@@ -0,0 +1,48 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners() {
+    // If the funtion is not defined with arrow funtion
+    // the "this" bellow will refer to the "(event)"parameter of the funtion
+    // by defining using and arrow funtion "this" to ther object
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = true;
+          break;
+        case 'ArrowRight':
+          this.right = true;
+          break;
+        case 'ArrowUp':
+          this.forward = true;
+          break;
+        case 'ArrowDown':
+          this.reverse = true;
+          break;
+      }
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = false;
+          break;
+        case 'ArrowRight':
+          this.right = false;
+          break;
+        case 'ArrowUp':
+          this.forward = false;
+          break;
+        case 'ArrowDown':
+          this.reverse = false;
+          break;
+      }
+    };
+  }
+}
